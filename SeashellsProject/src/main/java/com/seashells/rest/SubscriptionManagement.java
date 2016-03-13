a1ee25ba88e4afcb6558caf0a16376bc6dc04644
Natalie V <n.villan@gmail.com>
2016-03-13 17:24:42 -0400

    updatev3

--- SeashellsProject/src/main/java/com/seashells/rest/SubscriptionManagement.java
+++ SeashellsProject/src/main/java/com/seashells/rest/SubscriptionManagement.java
@@ -48,7 +48,10 @@ public class SubscriptionManagement {
 			}
 			in.close();
 	
-			return Response.status(responseCode).entity(response.toString()).build();
+			
+			String reponseString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><result><success>true</success><message>Account creation successful for Fake Co. by Alice</message><accountIdentifier>fakeco123</accountIdentifier></result>";
+			
+			return Response.status(responseCode).entity(reponseString).build();
 
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
