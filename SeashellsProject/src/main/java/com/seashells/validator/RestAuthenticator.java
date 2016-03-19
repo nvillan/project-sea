8544fbbd9bf77c20ce8ed143360fceec8919c439
Natalie V <n.villan@gmail.com>
2016-03-19 15:32:18 -0400

    auth header

--- SeashellsProject/src/main/java/com/seashells/validator/RestAuthenticator.java
+++ SeashellsProject/src/main/java/com/seashells/validator/RestAuthenticator.java
@@ -31,20 +31,23 @@ public class RestAuthenticator {
 	private static final String SIGNING_SECRET = "PtgVPwxQxkJ4ODA5";
 	private static final String SIGNING_KEY = "seashells-92212";
 
-	public boolean verify(HttpHeaders headers, String url) throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException{
+	public boolean verify(HttpHeaders headers, String url)
+			throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException {
 
-		Set<String> s =headers.keySet();
-		for (Iterator<String> iter = s.iterator(); iter.hasNext(); ) {
-		 
-			  String headerName = iter.next();
-			  System.out.println("" + headerName); 
-			}
-		
-		List<String> header = headers.get("oauth_signature");
-		if(header == null){
+
+
+		List<String> authHeader = headers.get("authorization");
+		if (authHeader == null) {
 			return false;
+		} else {
+			for (Iterator<String> iter = authHeader.iterator(); iter.hasNext();) {
+
+				String headerName = iter.next();
+				System.out.println("" + headerName);
+			}
 		}
-		String oAuthSignature = header.get(0);
+		// ("oauth_signature");
+		String oAuthSignature = authHeader.get(0);
 		System.out.println("The oAuthSignature in header : " + oAuthSignature);
 
 		OAuthConsumer consumer = new DefaultOAuthConsumer(SIGNING_KEY, SIGNING_SECRET);
@@ -52,7 +55,7 @@ public class RestAuthenticator {
 		String signedUrl = consumer.sign(url);
 
 		System.out.println("The signedUrl : " + signedUrl);
-		
+
 		return oAuthSignature.equals(signedUrl);
 	}
 
